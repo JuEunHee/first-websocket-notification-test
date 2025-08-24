e12c9e97b30284dfefc97d0b13df3f6a5574d4c1
EUNHEE JU <ehju222@gmail.com>
2025-08-25 03:51:00 +0900

    chore: Add docker compose file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EventsModule, NotificationsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // 개발용으로 true, 프로덕션에서는 false
+    }),
+    EventsModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
